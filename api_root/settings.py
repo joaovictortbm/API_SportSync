51d2476d1729780639d853475279e34110a005d2
Joao <joaovictormorais@yahoo.com.br>
2025-05-21 16:00:11 -0300

    Configura SECRET_KEY via .env e remove chave exposta

--- api_root/settings.py
+++ api_root/settings.py
@@ -22,16 +22,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7@#lfijy9=b@q#t4b&y(&83e$mtpu#m2bykdwu(t0-sd7ob$%q'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='.onrender.com').split(',')
 
 
 # Application definition
