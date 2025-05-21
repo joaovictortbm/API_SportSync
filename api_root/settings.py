0558ce4863281f69eff587266eb5fb11ccaad005
Joao <joaovictormorais@yahoo.com.br>
2025-05-21 15:16:44 -0300

    Segundo commit

--- api_root/settings.py
+++ api_root/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import dj_database_url
 import os
 
+from decouple import config
+
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
